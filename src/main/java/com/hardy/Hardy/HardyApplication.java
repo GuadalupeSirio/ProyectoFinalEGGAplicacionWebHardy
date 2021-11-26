5edc67e0a8e97736a75572543bae770bec8fc547
CARLOS LINERO <lineroacostacarlos@gmail.com>
2021-11-25 21:47:09 -0300

    primer commit mesa 9

---
+++ src/main/java/com/hardy/Hardy/HardyApplication.java
@@ -0,0 +1,13 @@
+package com.hardy.Hardy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HardyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HardyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hardy/Hardy/HardyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hardy.Hardy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HardyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
